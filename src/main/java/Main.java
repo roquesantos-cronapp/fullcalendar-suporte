9d6b6c179767ea91e1f385eaca52e94168ae8df9
Usuário de Teste <techne@techne.com.br>
2018-12-11 08:53:54 -0300

    commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Usuário de Teste
+ * @version 1.0
+ * @since 2018-12-11
+ *
+ */
+ 
+public class Main {
+
+	/**
+	 * Construtor
+	 **/
+	public Main (){
+	}
+
+}
